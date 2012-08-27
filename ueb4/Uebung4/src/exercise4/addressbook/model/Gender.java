55d9c461885b723ca5d754c01320124211d72d5a
Leopard2A5 <chummer_rene@gmx.net>
2012-08-27 17:01:51 +0200

    aded uebung 4

---
+++ ueb4/Uebung4/src/exercise4/addressbook/model/Gender.java
@@ -0,0 +1,15 @@
+/*
+ * Example code used in exercises for lecture "Grundlagen des Software-Testens"
+ * Created and given by Ina Schieferdecker and Edzard Hoefig
+ * Freie Universitaet Berlin, SS 2012
+ */
+package exercise4.addressbook.model;
+
+/**
+ * Captures the gender of a person: either Male or Female.
+ * @author Edzard Hoefig
+ * @see Entry
+ */
+public enum Gender {
+ Male, Female
+}
---
+++ ueb4/Uebung4/src/exercise4/test/TestConditionCoverage.java
@@ -0,0 +1,39 @@
+/*
+ * Example code used in exercises for lecture "Grundlagen des Software-Testens"
+ * Created and given by Ina Schieferdecker and Edzard Hoefig
+ * Freie Universitaet Berlin, SS 2012
+ */
+package exercise4.test;
+
+
+/**
+ * Uebung 4 - White Box Test
+ * Condition Coverage
+ * 
+ * Bitte Nummer der Gruppe eintragen:
+ * 0
+ * 
+ * Bitte Gruppenmitglieder eintragen:
+ * @author ...
+ */
+public class TestConditionCoverage {
+
+	/*
+     * Aufgabe 2a)
+     * Benutzen Sie JUnit um die Funktion des Filter AddressBookFilter.MALE_ADULTS_PHONE zu testen.
+     * Verwenden Sie zum Entwurf Ihrer Testfaelle die Strategie der Mehrfachbedingungsueberdeckung.
+     */
+    
+    /*
+     * Aufgabe 2b)
+     * Benutzen Sie JUnit um die Funktion des Filter AddressBookFilter.FEMALE_DUCK_OR_MAUS_OVER_30 zu testen.
+     * Verwenden Sie zum Entwurf Ihrer Testfaelle die Strategie der minimal bestimmenden Mehrfachbedingungsueberdeckung.
+     */
+    
+	/*
+     * Aufgabe 2c)
+     * Benutzen Sie JUnit um die Funktion des Filter AddressBookFilter.FEMALE_OR_MALE_KIDS_AND_SENIORS_WITH_D zu testen.
+     * Verwenden Sie zum Entwurf Ihrer Testfaelle die Strategie der einfachen Bedingungsueberdeckung.
+     */
+ 
+}
